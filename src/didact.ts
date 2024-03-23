b0e24b39e3c6707811908db7b93276d318ad8c16
elecdeer <37701077+elecdeer@users.noreply.github.com>
2024-03-23 16:57:30 +0900

    feat: step6で動くところまで

--- src/didact.ts
+++ src/didact.ts
@@ -49,21 +49,223 @@ function createTextElement(text: unknown) {
 	};
 }
 
+let nextUnitOfWork: Fiber | null = null;
+let currentRoot: Fiber | null = null;
+let wipRoot: Fiber | null = null;
+let deletions: Fiber[] = [];
+
 export function render(element: DidactElement, container: HTMLElement | Text) {
+	console.log("render", element, container);
+	wipRoot = {
+		dom: container,
+		props: {
+			children: [element],
+		},
+		alternate: currentRoot,
+	};
+
+	deletions = [];
+	nextUnitOfWork = wipRoot;
+}
+
+function commitRoot() {
+	commitWork(wipRoot?.child);
+	currentRoot = wipRoot;
+	wipRoot = null;
+}
+
+function commitWork(fiber: Fiber | undefined | null) {
+	if (!fiber) return;
+
+	const domParent = fiber.parent?.dom;
+
+	if (fiber.effectTag === "PLACEMENT" && fiber.dom != null) {
+		domParent?.appendChild(fiber.dom);
+	} else if (fiber.effectTag === "UPDATE" && fiber.dom != null) {
+		updateDom(fiber.dom, fiber.alternate?.props, fiber.props);
+	} else if (fiber.effectTag === "DELETION") {
+		domParent?.removeChild(fiber.dom);
+	}
+
+	commitWork(fiber.child);
+	commitWork(fiber.sibling);
+}
+
+const isEvent = (key: string) => key.startsWith("on");
+const isProperty = (key: string) => key !== "children" && !isEvent(key);
+const isNew = (prev: unknown, next: unknown) => (key: string) =>
+	prev[key] !== next[key];
+const isGone = (_prev: unknown, next: unknown) => (key: string) =>
+	!(key in next);
+
+function updateDom(dom: any, prevProps: unknown, nextProps: unknown) {
+	console.group("updateDom", dom, prevProps, nextProps);
+
+	// remove event listeners
+	Object.keys(prevProps)
+		.filter(isEvent)
+		.filter(isEvent)
+		.filter((key) => !(key in nextProps) || isNew(prevProps, nextProps)(key))
+		.forEach((name) => {
+			const eventType = name.toLowerCase().substring(2);
+			console.log("removeEventListener", eventType, prevProps[name]);
+			dom.removeEventListener(eventType, prevProps[name]);
+		});
+
+	// remove old properties
+	Object.keys(prevProps)
+		.filter(isProperty)
+		.filter(isGone(prevProps, nextProps))
+		.forEach((name) => {
+			console.log("removeProperty", name);
+			dom[name] = "";
+		});
+
+	// set new or changed properties
+	Object.keys(nextProps)
+		.filter(isProperty)
+		.filter(isNew(prevProps, nextProps))
+		.forEach((name) => {
+			console.log("setProperty", name, nextProps[name]);
+			dom[name] = nextProps[name];
+		});
+
+	// add event listeners
+	Object.keys(nextProps)
+		.filter(isEvent)
+		.filter(isNew(prevProps, nextProps))
+		.forEach((name) => {
+			const eventType = name.toLowerCase().substring(2);
+			console.log("addEventListener", eventType, nextProps[name]);
+			dom.addEventListener(eventType, nextProps[name]);
+		});
+
+	console.groupEnd();
+}
+
+export type Fiber = {
+	type?: "TEXT_ELEMENT" | string | undefined;
+	dom: any;
+	props: Record<string | number | symbol, unknown>;
+	parent?: Fiber;
+	child?: Fiber;
+	sibling?: Fiber;
+	alternate?: Fiber;
+};
+
+export function createDom(fiber: Fiber) {
 	const dom =
-		element.type === "TEXT_ELEMENT"
+		fiber.type === "TEXT_ELEMENT"
 			? document.createTextNode("")
-			: document.createElement(element.type);
+			: document.createElement(fiber.type);
 
-	for (const child of element.props.children) {
-		render(child, dom);
+	updateDom(dom, {}, fiber.props);
+	return dom;
+}
+
+function workLoop(deadline: IdleDeadline) {
+	let shouldYield = false;
+
+	while (nextUnitOfWork && !shouldYield) {
+		nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+		shouldYield = deadline.timeRemaining() < 1;
 	}
 
-	const isProperty = (key: string) => key !== "children";
-	for (const name of Object.keys(element.props).filter(isProperty)) {
-		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-		(dom as any)[name] = element.props[name];
+	if (!nextUnitOfWork && wipRoot) {
+		commitRoot();
 	}
 
-	container.appendChild(dom);
+	requestIdleCallback(workLoop);
+}
+
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(fiber: Fiber): Fiber | null {
+	// add dom node
+	if (!fiber.dom) {
+		fiber.dom = createDom(fiber);
+	}
+
+	// if (fiber.parent) {
+	// 	fiber.parent.dom.appendChild(fiber.dom);
+	// }
+
+	// create new fibers
+	const elements = fiber.props.children;
+	reconcileChildren(fiber, elements);
+
+	// return next unit of work
+	if (fiber.child) {
+		return fiber.child;
+	}
+	let nextFiber = fiber;
+	while (nextFiber) {
+		if (nextFiber.sibling) {
+			return nextFiber.sibling;
+		}
+		nextFiber = nextFiber.parent;
+	}
+	return null;
+}
+
+function reconcileChildren(wipFiber: Fiber, elements: Fiber[]) {
+	let index = 0;
+	let oldFiber = wipFiber.alternate?.child;
+	let prevSibling: Fiber | null = null;
+
+	while (index < elements.length || oldFiber != null) {
+		const element = elements[index];
+		let newFiber: Fiber | null = null;
+
+		const sameType = oldFiber && element && element.type === oldFiber.type;
+
+		if (sameType) {
+			// update the node
+			newFiber = {
+				// biome-ignore lint/style/noNonNullAssertion: sameTypeでチェック済
+				type: oldFiber!.type,
+				props: element.props,
+				// biome-ignore lint/style/noNonNullAssertion: sameTypeでチェック済
+				dom: oldFiber!.dom,
+				parent: wipFiber,
+				alternate: oldFiber,
+				effectTag: "UPDATE",
+			} satisfies Fiber;
+		}
+
+		if (element && !sameType) {
+			// add new node
+			newFiber = {
+				type: element.type,
+				props: element.props,
+				parent: wipFiber,
+				dom: null,
+				alternate: undefined,
+				effectTag: "PLACEMENT",
+			} satisfies Fiber;
+		}
+
+		if (oldFiber && !sameType) {
+			// delete the old node
+
+			oldFiber.effectTag = "DELETION";
+			deletions.push(oldFiber);
+		}
+
+		if (oldFiber) {
+			oldFiber = oldFiber.sibling;
+		}
+
+		if (index === 0) {
+			wipFiber.child = newFiber;
+		} else {
+			if (prevSibling) {
+				prevSibling.sibling = newFiber;
+			}
+		}
+
+		prevSibling = newFiber;
+
+		index++;
+	}
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,29 @@
 import * as Didact from "./didact";
 
-const element = (
-	<div id="foo">
-		<a href="./">bar</a>
-		<b>bold!</b>
-	</div>
-);
+const container = document.getElementById("root");
+if (container === null) throw new Error("root element not found");
+
+const updateValue = (e: any) => {
+	console.log("updateValue", e.target.value);
+	rerender(e.target.value);
+};
+
+const rerender = (value: string) => {
+	console.log("rerendering with value", value);
+	const element = (
+		<div id="foo">
+			<a href="./">bar</a>
+			<br />
+			<input onInput={updateValue} value={value} />
+			<br />
+			<b>{value}</b>
+		</div>
+	);
+
+	Didact.render(element, container);
+};
+
+rerender("hello");
 
 // const element = Didact.createElement(
 //   "div",
@@ -13,7 +31,3 @@ const element = (
 //   Didact.createElement("a", null, "bar"),
 //   Didact.createElement("b")
 // );
-
-const container = document.getElementById("root");
-if (container === null) throw new Error("root element not found");
-Didact.render(element, container);
